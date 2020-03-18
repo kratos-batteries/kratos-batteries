1e1619b669f8f85f95de5fa1c13ac97eaeed7d6a
Yousef Baioumy <baioumyy@uw.edu>
2020-03-17 22:59:20 -0700

    Fixed file path so that the unit test would work

--- kratosbat/tests/test_nn.py
+++ kratosbat/tests/test_nn.py
@@ -7,7 +7,7 @@ import kratosbat.NN.nn as nn
 
 def test_nn_capacity():
     """Test for nn_capacity"""
-    model = nn.nn_capacity('NEWTrainingData_MinMaxScaler.csv',
+    model = nn.nn_capacity('../Data/NEWTrainingData_MinMaxScaler.csv',
                             115, 100, 75, 2, 4000)
     assert isinstance(model, torch.nn.modules.container.Sequential),\
         "Output is not the right type"
@@ -15,7 +15,7 @@ def test_nn_capacity():
 
 def test_nn_volume():
     """Test for nn_volume"""
-    model = nn.nn_volume('NEWTrainingData_MinMaxScaler.csv',
+    model = nn.nn_volume('../Data/NEWTrainingData_MinMaxScaler.csv',
                             2, 100, 75, 1, 4000)
     assert isinstance(model, torch.nn.modules.container.Sequential),\
         "Output is not the right type"
