8b4ad017179d05e971f04fe4924a9041fb7aaa4e
baioumyy <59420999+baioumyy@users.noreply.github.com>
2020-03-18 02:21:51 -0700

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -37,10 +37,23 @@ release = 'March 18, 2020'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon',
+              'sphinx.ext.coverage']
+
+# Napoleaon settings
+napoleon_numpy_docstring = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
